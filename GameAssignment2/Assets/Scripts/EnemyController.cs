d2b53eb8e5a1ac676031a55690db815f83361141
PaulvDeursen <paulvdeursen@gmail.com>
2019-02-18 16:48:28 +0100

    Renamed a few things

--- GameAssignment2/Assets/Scripts/EnemyController.cs
+++ GameAssignment2/Assets/Scripts/EnemyController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EenemyController : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
 	public static Color friendlyColor = new Color(106, 255, 106) / 255;
 	public static Color enemyColor = new Color(255, 106, 106) / 255;
@@ -10,13 +10,16 @@ public class EenemyController : MonoBehaviour
  	private bool isNeutral = false;
  	private Light light;
 
+    public Transform player;
+    public int enemySpeed;
+
     void Start() {
         light = GetComponent<Light>();
         MakeEnemy();
     }
 
     void Update() {
-        
+        followPlayer();
     }
 
     void OnCollisionEnter(Collision col) {
@@ -33,5 +36,12 @@ public class EenemyController : MonoBehaviour
     private void MakeNeutral() {
     	light.color = friendlyColor;
     	isNeutral = true;
+    }
+
+    private void followPlayer()
+    {
+        //Calculate distance
+        Vector3 distance = transform.position - player.position;
+        transform.Translate(-distance * enemySpeed * Time.deltaTime);
     }
 }
